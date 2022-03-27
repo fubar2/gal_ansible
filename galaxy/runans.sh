4f4ee550bf1fc6994c097c35394849d69165cb25
fubar2 <ross.lazarus@gmail.com>
2022-03-27 22:28:41 +1100

    based on git-gat and working more or less for galaxy - toolshed todo

--- galaxy/runans.sh
+++
@@ -1,9 +0,0 @@
-# make an unencrypted secret - will be encrypted before save
-printf "vault_id_secret: " > group_vars/secret.yml
-openssl rand -base64 24 > .vault-password.txt
-openssl rand -base64 24 > .vault-id-secret.txt
-echo ./.vault-id-secret.txt >> group_vars/secret.yml
-ansible-vault encrypt --encrypt-vault-id default --vault-password-file .vault-password.txt group_vars/secret.yml
-ansible-galaxy install -p roles -r requirements.yml
-ansible-playbook  galaxy.yml --vault-password-file ./.vault-password.txt
-
